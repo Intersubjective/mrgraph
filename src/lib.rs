c0eb60e0ec7b8caa96e9e88bece1f262073fcb9a
shestero <Michael Shestero>
2024-03-25 01:09:52 +0100

    -v0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,15 @@ pub use mrgraph::GRAPH;
 
 pub mod error;
 
-mod meritrank {
+pub mod meritrank {
     pub struct MeritRank;
     pub struct MeritRankError;
     pub struct MyGraph;
     pub struct Weight;
+    pub mod node {
+        pub struct Node;
+    }
 }
+
 
 
